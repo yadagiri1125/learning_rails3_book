b409f83a3c8553cb9e00ad9aa38e76fed8c6a17e
yadagiri Tandu <yadagiri1125@gmail.com>
2014-12-30 22:40:15 +0530

    development changes related to home controller and index vie

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helloo1::Application.config.secret_token = '87f6765e9c8592c2b660435d76835c30c6ad3d9a99eecec5f3609b10b026fda6836ef62f80b1fe5fb60ecd63dd8f645313f9a89a20c3941809fedcfa716c2692'
